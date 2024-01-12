771e6fb07600fd2c7f650ee895ed748d2f747317
Marco Gesteira <mgesteira@outlook.com>
2024-01-11 22:29:17 -0300

    Começando a implementar o tabuleiro

---
+++ Chess-console/Tabuleiro/Peca.cs
@@ -0,0 +1,20 @@
+﻿using Tabuleiro.Enums;
+
+namespace Tabuleiro
+{
+    internal class Peca
+    {
+        public Posicao Posicao { get; set; }
+        public Cor Cor { get; protected set; }
+        public int QtdMovimentos { get; protected set; }
+        public Tabuleiro Tabuleiro { get; set; }
+
+        public Peca(Posicao posicao, Cor cor, Tabuleiro tabuleiro)
+        {
+            this.Posicao = posicao;
+            this.Cor = cor;
+            this.Tabuleiro = tabuleiro;
+            this.QtdMovimentos = 0;
+        }
+    }
+}
--- Chess-console/Tabuleiro/Posicao.cs
+++ Chess-console/Tabuleiro/Posicao.cs
@@ -1,5 +1,4 @@
-﻿
-namespace Tabuleiro
+﻿namespace Tabuleiro
 {
     internal class Posicao
     {
