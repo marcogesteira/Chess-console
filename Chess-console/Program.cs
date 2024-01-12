0a1edbaebfdce25872c6a3758dae5e7a643335e9
Marco Gesteira <mgesteira@outlook.com>
2024-01-11 22:46:52 -0300

    Começando a implementar o método que imprime o tabuleiro.

--- Chess-console/Program.cs
+++ Chess-console/Program.cs
@@ -1,10 +1,15 @@
-﻿namespace Chess_console
+﻿using System;
+using Tabuleiro;
+
+namespace Chess_console
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Tabuleiro.Tabuleiro tabuleiro = new Tabuleiro.Tabuleiro(8, 8);
+
+            Tela.imprimirTabuleiro(tabuleiro);
         }
     }
 }
---
+++ Chess-console/Tela.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Tabuleiro;
+
+namespace Chess_console
+{
+    internal class Tela
+    {
+        public static void imprimirTabuleiro(Tabuleiro.Tabuleiro tab)
+        {
+            for (int i = 0; i < tab.Linhas; i++)
+            {
+                for (int j = 0; j < tab.Colunas; j++)
+                {
+                    if (tab.Peca(i,j) == null)
+                    {
+                        Console.Write("- ");
+                    }
+                    else
+                    {
+                        Console.Write(tab.Peca(i, j) + " ");
+                    }
+                    
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
